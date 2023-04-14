0e7e22358265e48152289d00aa8d0b18f75281e9
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-04-14 17:53:24 +0200

    add fake flats api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat';
 
-function App() {
-  const flat = {
-    "name": "Cool place in the woods",
-    "imageUrl": "https://images.unsplash.com/photo-1437996533264-7a6b3f2f7dfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
-    "price": 200,
-    "priceCurrency": "EUR"
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
 
-  return (
-    <div className="app">
-      <div className="main">
-        <div className="flats">
-          <Flat flat={flat} />
-          <Flat flat={flat} />
-          <Flat flat={flat} />
-          <Flat flat={flat} />
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
         </div>
+        <div className="map"></div>
       </div>
-      <div className="map"></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
